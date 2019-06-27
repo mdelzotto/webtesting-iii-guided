85a808120e599e6e73ad96b46a31eec30679f17f
mdelzotto <madelzotto@comcast.net>
2019-06-27 11:10:47 -0400

    testing react guided

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Speaker from './Speaker';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    message: 'nothing to say'
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Speaker speak={this.speak} message={this.state.message} />
       </div>
     );
   }
+
+  speak = () => {
+    this.setState({ message: 'you are not mocking me' });
+  };
 }
 
 export default App;
